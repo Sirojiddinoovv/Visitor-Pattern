240004df087b75dfd38b5e6cbee47a4f29581f27
nodir29 <nodir.sirojiddinov@smartbank.uz>
2025-08-01 15:56:32 +0500

    added exception handler config

---
+++ src/main/kotlin/uz/nodir/collateral/exceptions/IncorrectDataException.kt
@@ -0,0 +1,10 @@
+package uz.nodir.collateral.exceptions
+
+/**
+ * Author:Nodir
+ * User:smart
+ * Date:7/31/2025
+ * Time:9:21 AM
+ */
+
+class IncorrectDataException(message: String): RuntimeException()
---
+++ src/main/kotlin/uz/nodir/collateral/exceptions/RestAdvice.kt
@@ -0,0 +1,54 @@
+package uz.nodir.collateral.exceptions
+
+import mu.KotlinLogging
+import org.springframework.context.annotation.Configuration
+import org.springframework.http.ResponseEntity
+import org.springframework.http.converter.HttpMessageNotReadableException
+import org.springframework.web.bind.MethodArgumentNotValidException
+import org.springframework.web.bind.annotation.ExceptionHandler
+import uz.nodir.collateral.model.dto.core.response.CoreError
+import uz.nodir.collateral.model.dto.core.response.ResultData
+
+/**
+ * Author:Nodir
+ * User:smart
+ * Date:8/1/2025
+ * Time:3:52 PM
+ */
+
+@Configuration
+class RestAdvice {
+
+    private val log = KotlinLogging.logger {}
+
+    @ExceptionHandler(value = [MethodArgumentNotValidException::class])
+    fun methodArgumentNotValidException(e: MethodArgumentNotValidException): ResponseEntity<ResultData<CoreError>> {
+        log.error("MethodArgumentNotValidException message: {}", e.message)
+        return ResponseEntity.ok(
+            ResultData.error(
+                400, "Fields wrong filled!"
+            )
+        )
+    }
+
+    @ExceptionHandler(value = [HttpMessageNotReadableException::class])
+    fun httpMessageNotReadableException(e: HttpMessageNotReadableException): ResponseEntity<ResultData<CoreError>> {
+        log.error("HttpMessageNotReadableException message: {}", e.message)
+        return ResponseEntity.ok(
+            ResultData.error(
+                400, "Fields wrong filled!"
+            )
+        )
+    }
+
+    @ExceptionHandler(value = [IncorrectDataException::class])
+    fun incorrectDataException(e: IncorrectDataException): ResponseEntity<ResultData<CoreError>> {
+        log.error("IncorrectDataException message: {}", e.message)
+        return ResponseEntity.ok(
+            ResultData.error(
+                400, e.message ?: "IncorrectDataException message"
+            )
+        )
+    }
+
+}
